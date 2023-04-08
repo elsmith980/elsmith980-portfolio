f2d33d1c7163e88ae4d3b28b7a6b4634ec23fabe
Ella Smith <elsmith980@gmail.com>
2023-04-07 21:56:31 -0400

    about page tabs

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,75 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
+import React, { useState } from "react";
+
+const DegreeInfo = () => (
+  <div className="panel-block my-3">
+    <figure className='image is-64x64 is-rounded is-pulled-left'>
+      <Image
+        src="/waterloologo.png"
+        layout="fill"
+      />
+    </figure>
+    <div className="has-text-centered mx-auto is-pulled-right"> 
+        <p className="is-size-6 has-text-weight-bold">Bachelor of Applied Science </p>
+        <p className="is-size-6 has-text-weight-bold">Honours Computer Engineering, Co-op</p>
+        <p className="is-size-6">University of Waterloo</p>
+        <p className="is-size-7 mt-3">Expected Graduation: June 2025</p>
+        <p className="is-size-7">Average: ---</p>
+    </div>            
+  </div>
+)
+
+const Courses = () => (
+  <div className="panel-block my-3">
+    Courses 
+  </div>
+)
+
+const Awards = () => (
+  <div className="panel-block my-3">
+    Awards
+  </div>
+)
+
+const Extracurr = () => (
+  <div className="panel-block my-3">
+    Extracurriculars
+  </div>
+)
+
+const General = () => (
+  <div className="panel-block my-3">
+    General
+  </div>
+)
+
+const Languages = () => (
+  <div className="panel-block my-3">
+    Languages
+  </div>
+)
+
+const Libraries = () => (
+  <div className="panel-block my-3">
+    Libraries
+  </div>
+)
+
+const Tools = () => (
+  <div className="panel-block my-3">
+    Tools
+  </div>
+)
 
 export default function About() {
+    const educationTabs = ['Degree', 'Courses', 'Awards', 'Extracurriculars'];
+    const [activeEduTab, setEduTab] = React.useState('Degree');
+    const skillsTabs = ['General', 'Languages', 'Libraries & Frameworks', 'Tools'];
+    const [activeSkillsTab, setSkillsTab] = React.useState('General');
+
     return (
       <div className="page-container">
         <Head>
@@ -20,50 +87,52 @@ export default function About() {
               🌿 Passionate about sustainability and building inclusive tech that improves lives
             </p>
           </div>
-          <div className="columns">
+          <div className="columns" style={{minHeight: '300px'}}>
             <nav className="panel is-warning column is-half mx-2">
               <p className="panel-heading">
                 Education
               </p>
-            <p className="panel-tabs">
-              <a className="is-active">Degree</a>
-              <a>Courses</a>
-              <a>Awards</a>
-              <a>Extracurriculars</a>
-            </p>
-            <div className="panel-block has-text-left is-active">
-              <span className="panel-icon">
-                <i className="fas fa-book" aria-hidden="true"></i>
-              </span>
-              <div className="has-text-centered mx-auto"> 
-                  <p className="is-size-6">Bachelor of Applied Science</p>
-                  <p className="is-size-6">Computer Engineering Co-op</p>
-                  <p className="is-size-6">University of Waterloo</p>
-                  <p className="is-size-7 mt-3">Graduation: June 2025</p>
-                  <p className="is-size-7">Average: ---</p>
-              </div>            
-            </div>
-          </nav>
-        <nav className="panel is-warning column is-half mx-2">
-            <p className="panel-heading">
-              Skills
-            </p>
-          <p className="panel-tabs px-2">
-            <a className="is-active">General</a>
-            <a>Languages</a>
-            <a>Libraries & Frameworks</a>
-            <a>Tools</a>
-          </p>
-          <div className="panel-block is-active">
-            <span className="panel-icon">
-              <i className="fas fa-book" aria-hidden="true"></i>
-            </span>
-            bulma
-          </div>
-        </nav>
-        </div>
-      </main>
+              <p className="panel-tabs">
+                {educationTabs.map((tab) => (
+                  <a 
+                    className={`${activeEduTab!=tab ? "" : "is-active has-text-weight-semibold"} px-4`}  
+                    key={tab}
+                    onClick={() => setEduTab(tab)}
+                  >
+                    {tab}
+                  </a>
+                ))}
+              </p>
 
+              { activeEduTab=='Degree' ? <DegreeInfo /> : null }
+              { activeEduTab=='Courses' ? <Courses /> : null }
+              { activeEduTab=='Awards' ? <Awards /> : null }
+              { activeEduTab=='Extracurriculars' ? <Extracurr /> : null }
+            </nav>
+            <nav className="panel is-warning column is-half mx-2">
+              <p className="panel-heading">
+                Skills
+              </p>
+              <p className="panel-tabs">
+                {skillsTabs.map((tab) => (
+                  <a 
+                    className={`${activeSkillsTab!=tab ? "" : "is-active has-text-weight-semibold"} px-4`}  
+                    key={tab}
+                    onClick={() => setSkillsTab(tab)}
+                  >
+                    {tab}
+                  </a>
+                ))}
+              </p>
+
+              { activeSkillsTab=='General' ? <General /> : null }
+              { activeSkillsTab=='Languages' ? <Languages /> : null }
+              { activeSkillsTab=='Libraries & Frameworks' ? <Libraries /> : null }
+              { activeEduTab=='Tools' ? <Tools /> : null }
+
+            </nav>
+          </div>
+        </main>
       </div>
     )
   }
